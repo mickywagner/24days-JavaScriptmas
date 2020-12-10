d79c9846e0c5eae965d1f2f6dc0a2a7098339398
Michaela Wagner <mickywagner@gmail.com>
2020-12-10 02:06:09 -0700

    Day 5 solution added

---
+++ day5_reverseString/main.js
@@ -0,0 +1,24 @@
+function reverseAString(str) {
+    return str.split('').reverse().join('')
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('reverseAString()', () => {
+    it('returns original string reversed', () => {
+        // arrange
+        const str = 'hello';
+        
+        // act
+        const result = reverseAString(str);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toBe('olleh');
+    });
+});
