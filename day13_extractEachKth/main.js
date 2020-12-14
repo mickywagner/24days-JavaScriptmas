ba88f188e3e1bd656f82b1b2e351a7901789b99a
Michaela Wagner <mickywagner@gmail.com>
2020-12-13 20:37:19 -0700

    Add day 13 solution

---
+++ day13_extractEachKth/main.js
@@ -0,0 +1,25 @@
+function extractEachKth(nums, index) {
+    return nums.filter(num => num % index !== 0)
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('extractEachKth()', () => {
+    it('returns largest positive integer possible for digit count', () => {
+        // arrange
+        const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const index = 3;
+        
+        // act
+        const result = extractEachKth(nums, index);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual([1, 2, 4, 5, 7, 8, 10]);
+    });
+});
