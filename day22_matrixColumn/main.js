f896846722d62086f4c2c622a6bb28ae7584fca1
Michaela Wagner <mickywagner@gmail.com>
2020-12-22 23:39:04 -0700

    Add day 22 solution

---
+++ day22_matrixColumn/main.js
@@ -0,0 +1,44 @@
+function extractMatrixColumn(matrix, column) {
+   let matrixArray = []
+   matrix.map(array => matrixArray.push(array[column]))
+   return matrixArray
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('extractMatrixColumn()', () => {
+    it('returns largest positive integer possible for digit count', () => {
+        // arrange
+        const matrix = [[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]];
+        const column = 2;
+        
+        // act
+        const result = extractMatrixColumn(matrix, column);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual([1, 0, 3]);
+    });
+});
+
+describe('extractMatrixColumn()', () => {
+    it('returns largest positive integer possible for digit count', () => {
+        // arrange
+        const matrix = [[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]];
+        const column = 1;
+        
+        // act
+        const result = extractMatrixColumn(matrix, column);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual([1, 5, 1]);
+    });
+});
